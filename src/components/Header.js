fc3c02f167318bc67a141e990de74aa401066cd0
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-10-01 23:10:53 -0700

    more updates of home page content

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <nav>
-      <NavLink className="btn btn-outline-secondary m-2" to="/home">Home</NavLink>
-      <NavLink className="btn btn-outline-secondary m-2" to="/books">Info</NavLink>
-      <NavLink className="btn btn-outline-secondary m-2" to="/about">About</NavLink>
+    <nav className="text-center">
+      <NavLink className="btn btn-outline-dark m-2" to="/home">Home</NavLink>
+      <NavLink className="btn btn-outline-dark m-2" to="/books">Info</NavLink>
+      <NavLink className="btn btn-outline-dark m-2" to="/about">About</NavLink>
       <img className="m-2" src="images/reddot-award.jpg" alt="red dot award 2017, recognizing the scooter for having the best quality" style={{width:'80px', left: '550px'}}/>
     </nav>
    
--- src/components/Home.js
+++ src/components/Home.js
@@ -55,35 +55,25 @@ const Home = () => (
             <br />
             <br />
             <p style={{fontSize: "12px", color: '#c2c2d6'}}>Weighs just 26.9 lbs</p>
-            <p className="mx-5 px-5" style={{fontSize: "11px"}}>The sturdy frame is made of aerospace-grade aluminum, which has a low density and high structural strength. It also has excellent thermal conductivity and corrosion resistance for added durability. At just 26.9 lbs, Mi Electric Scooter is a great portable size and weight for carry-ons and for maneuvering stairs and pavements.</p>
+            <p className="mx-5 px-5" style={{fontSize: "11px", maxWidth: '1000px'}}>The sturdy frame is made of aerospace-grade aluminum, which has a low density and high structural strength. It also has excellent thermal conductivity and corrosion resistance for added durability. At just 26.9 lbs, Mi Electric Scooter is a great portable size and weight for carry-ons and for maneuvering stairs and pavements.</p>
         </div>
         <div className="m-0" id="scoot1" >
             <img src='images/person-carrying-scooter.jpg' />
         </div>
 
-        <div class="container" style={{position: "absolute"}}>
-            <div class="row">
-                <div className="col-5 m-0 p-0 text-black">
-                    <ul className="p-2" style={{ fontSize: "11px", listStyleType: "none"}}>
-                        <li>
-                            <span style={{fontWeight: "bold"}}>Anti-slip handle</span>
-                            <p>Provides a great grip and control over the scooter, even for longer rides.</p>
-                        </li>
-                        <li>
-                        <span style={{fontWeight: "bold"}}>Controlled acceleration</span>
-                            <p>Give the accelerator a gentle downward push and adjust the amount of pressure until you reach a comfortable speed.</p>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-6"></div>
-            </div>
+        <div className="text-center m-4">
+            <a className="btn btn-dark" style={{width: '200px'}}>Learn More</a>
         </div>
+        <div className="text-center m-4">
+            <a className="btn btn-dark" style={{width: '200px'}}>buy Now</a>
+        </div>
+        
 
-        <div className="m-0" id="scoot1" style={{backgroundColor: "#f3f3f3"}}>
+        {/* <div className="m-0" id="scoot1" style={{backgroundColor: "#f3f3f3"}}>
             
             
 
-            <div class="container">
+            <div class="container" >
                 <div class="row">
                     <div class="col-1" ></div>
                     <div class="col-8">
@@ -97,9 +87,25 @@ const Home = () => (
                 </div>
             </div>
 
+            <div class="container" style={{position: "absolute"}}>
+                <div class="row">
+                    <div className="col-5 m-0 p-0 text-black">
+                        <ul className="p-2" style={{ fontSize: "11px", listStyleType: "none"}}>
+                            <li>
+                                <span style={{fontWeight: "bold"}}>Anti-slip handle</span>
+                                <p>Provides a great grip and control over the scooter, even for longer rides.</p>
+                            </li>
+                            <li>
+                            <span style={{fontWeight: "bold"}}>Controlled acceleration</span>
+                                <p>Give the accelerator a gentle downward push and adjust the amount of pressure until you reach a comfortable speed.</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-6"></div>
+                </div>
+            </div>
             
-            
-        </div>
+        </div> */}
 
         
 
